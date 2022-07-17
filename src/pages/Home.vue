b81806d305f52f1eaa160d21c9d924d608d3658b
hassan <hzangene5@gmail.com>
2022-07-17 20:04:40 +0430

    home page done

---
+++ src/pages/Home.vue
@@ -0,0 +1,26 @@
+<template>
+<div class="container mt-5">
+    <div class="row">
+        <div class="col-md-6">
+        
+        <p>   Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facilis sed, unde corporis ducimus 
+           adipisci eum distinctio quia sapiente. Corrupti, nam est natus ut magnam necessitatibus animi
+            nulla mollitia atque omnis.</p>
+        </div>
+    </div>
+</div>
+</template>
+
+<script>
+
+
+export default {
+
+name
+
+}
+</script>
+
+<style>
+
+</style>
--- src/router.js
+++ src/router.js
@@ -1,1 +1,14 @@
-import {createRouter , createWebHistory} from 'vue-router'
+import {createRouter , createWebHistory} from 'vue-router';
+import Home from './pages/Home.vue';
+
+const routes = [
+       {path : "/" ,name : "HomePage" , component : Home}
+];
+
+const router = createRouter({
+       history : createWebHistory(),
+       routes
+
+});
+
+export default router;
